e329bf342a9ae3c5f25756adc56168fbae5d7346
Dmitrii Mazurok <tmazurok@gmail.com>
2023-01-10 23:02:08 +0300

    add seo component

--- pages/shows/[id].tsx
+++ pages/shows/[id].tsx
@@ -3,6 +3,7 @@ import { GetServerSidePropsContext } from "next";
 import { showsAPI } from "src/shared/api/shows";
 import { TvShowDetail } from "src/shared/entities/tv-show";
 import { ShowDetailPage } from "src/pages/routes/show-detail";
+import { SEO } from "src/shared/ui/seo";
 
 export default function ShowDetail({
   id,
@@ -15,16 +16,19 @@ export default function ShowDetail({
   redirectToHomeonBack,
 }: TvShowDetail & { redirectToHomeonBack: boolean }) {
   return (
-    <ShowDetailPage
-      id={id}
-      name={name}
-      cast={cast}
-      genres={genres}
-      summary={summary}
-      image={image}
-      rating={rating}
-      redirectToHomeonBack={redirectToHomeonBack}
-    />
+    <>
+      <SEO title={`TV Maze | ${name}`} />
+      <ShowDetailPage
+        id={id}
+        name={name}
+        cast={cast}
+        genres={genres}
+        summary={summary}
+        image={image}
+        rating={rating}
+        redirectToHomeonBack={redirectToHomeonBack}
+      />
+    </>
   );
 }
 
